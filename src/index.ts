3f9fab2dba6b7dd89af5fa9111e93332dda1ef8e
madhushani-29 <madhushani2904@gmail.com>
2024-11-09 18:40:12 +0530

    unwanted files removed

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,7 @@ app.get("/health", async (req: Request, res: Response) => {
   res.status(200).json({ message: "Health is ok" });
 });
 
- app.use("/api/user", UserRoutes);
-// app.use("/api/my/restaurant", MyRestaurantRoutes);
-// app.use("/api/restaurant", RestaurantRoutes);
-// app.use("/api/order", OrderRoutes);
+app.use("/api/user", UserRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
