9899decdb1fee047679f2f5e17a2a2c90698994e
madhushani-29 <madhushani2904@gmail.com>
2024-11-23 13:08:30 +0530

    patch operation add to cors

--- dist/index.js
+++ dist/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", req.headers.origin);
     }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     if (req.method === "OPTIONS") {
         return res.status(200).end();
     }
